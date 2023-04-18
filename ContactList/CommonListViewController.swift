8ddaa9029b55751fc4146b50758a208e80d0720c
polosa00 <i@apolochanin.ru>
2023-04-18 19:58:59 +0300

    refactoring code in CommonVC

--- ContactList/CommonListViewController.swift
+++ ContactList/CommonListViewController.swift
@@ -7,31 +7,28 @@
 
 import UIKit
 
-class CommonListViewController: UITableViewController {
+final class CommonListViewController: UITableViewController {
 
-        
     let contactSList = Person.getPerson()
-    
 
     // MARK: - UITableViewDataSource
-
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return contactSList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
     }
 
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         let contact = contactSList[section]
-
         return contact.fullName
     }
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 2
-    }
+    
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         60
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "sectionCell", for: indexPath)
         var content = cell.defaultContentConfiguration()
